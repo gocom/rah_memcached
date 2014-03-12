4bd503e34313152adc584a02cb2de5b5a2bcfdbb
Jukka Svahn <web@rahinaa.biz>
2014-03-12 21:28:48 +0200

    Initial commit.

---
+++ src/Rah/Memcached.php
@@ -0,0 +1,95 @@
+<?php
+
+/*
+ * rah_memcached - Memcached templates for Textpattern CMS
+ * https://github.com/gocom/rah_memcached
+ *
+ * Copyright (C) 2014 Jukka Svahn
+ *
+ * This file is part of rah_memcached.
+ *
+ * rah_memcached is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2.
+ *
+ * rah_memcached is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rah_flat. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Extends Memcached with Textpattern installation specific storage.
+ *
+ * Configuration happens by defining few constants in config.php:
+ *
+ * <code>
+ * define('RAH_MEMCACHED_HOST', 'localhost');
+ * define('RAH_MEMCACHED_PORT', 11211);
+ * </code>
+ */
+
+class Rah_Memcached extends Memcached
+{
+    /**
+     * A prefix used to seperate Textpattern installation keys from one another.
+     *
+     * This prefix allows sharing same Memcached server between multiple
+     * Textpattern sites.
+     *
+     * @var string
+     */
+
+    protected $rahKeyPrefix;
+
+    /**
+     * {@inheritdoc}
+     */
+
+    public function __construct()
+    {
+        $this->rahKeyPrefix = 'Rah:Textpattern:'.get_pref('version').':'.get_pref('siteurl').':';
+        parent::__construct();
+        $this->addServer(RAH_MEMCACHED_HOST, RAH_MEMCACHED_PORT);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+
+    public function addServer($host , $port)
+    {
+        $servers = $this->memcached->getServerList();
+
+        if (is_array($servers)) {
+            foreach ($servers as $server) {
+                if ($server['host'] == $host and $server['port'] == $port) {
+                    return true;
+                }
+            }
+        }
+
+        return $this->memcached->addServer($host , $port);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+
+    public function set($key, $value, $expiration = 0)
+    {
+        return parent::get($this->rahKeyPrefix . $key, $value, $expiration);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+
+    public function get($key)
+    {
+        return parent::get($this->rahKeyPrefix . $key);
+    }
+}
---
+++ src/Rah/Tag.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * rah_memcached - Memcached templates for Textpattern CMS
+ * https://github.com/gocom/rah_memcached
+ *
+ * Copyright (C) 2014 Jukka Svahn
+ *
+ * This file is part of rah_memcached.
+ *
+ * rah_memcached is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2.
+ *
+ * rah_memcached is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rah_flat. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Stores template portions in Memcached.
+ *
+ * @param  array  $atts  Attributes
+ * @param  string $thing Contained statement
+ * @return string User markup
+ *
+ * <code>
+ * <txp:rah_memcached name="section_nav">
+ *  <txp:section_list>
+ *      <txp:section />
+ *  </txp:section_list>
+ * </txp:rah_memcached>
+ * </code>
+ *
+ * If used as a single tag, it fetched a value from the cache by
+ * the key name.
+ *
+ * <code>
+ * <txp:rah_memcached name="section_nav" />
+ * </code>
+ */
+
+function rah_memcached($atts, $thing = null)
+{
+    static $memcached = null;
+
+    extract(lAtts(array(
+        'expires' => 3600,
+        'name'    => null,
+    ), $atts));
+
+    if (!$name) {
+        trigger_error(gTxt('invalid_attribute_value', array('{name}' => 'name')));
+        return '';
+    }
+
+    if ($memcached === null) {
+        $memcached = new Rah_Memcached();
+    }
+
+    if ($thing === null) {
+        return (string) $memcached->get($name);
+    }
+
+    if (($value = $memcached->get($name)) !== false) {
+        return $value;
+    }
+
+    $value = (string) parse($thing);
+    $memcached->set($name, $value, (int) $expires);
+
+    return $value;
+}
