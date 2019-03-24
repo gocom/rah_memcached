9312934b983e90eb4e1a846da8b8a061ab828a90
Jukka Svahn <void@rah.pw>
2019-03-24 18:48:21 +0200

    Register the tag

---
+++ src/Index.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * rah_memcached - Memcached templates for Textpattern CMS
+ * https://github.com/gocom/rah_memcached
+ *
+ * Copyright (C) 2019 Jukka Svahn
+ *
+ * This file is part of rah_memcached.
+ *
+ * rah_memcached is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2.
+ *
+ * rah_memcached is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rah_memcached. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+Txp::get('\Textpattern\Tag\Registry')->register('rah_memcached', 'rah_memcached');
